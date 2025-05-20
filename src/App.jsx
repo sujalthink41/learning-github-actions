1237e0fd80d2e8084bd04356e9869d04e11d7e98
Dev41 <dev41@example.com>
2025-05-20 14:07:58 +0530

    Added some codes in the design

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ const App = () => {
     <div className='text-3xl font-bold underline text-center'>Hello World</div>
 
     <p className='text-center text-4xl text-amber-500'>Hello There how are you all </p>
+
+    <h1>Programming Languages</h1>
+    <ul className='list-disc list-inside'>
+      <li className='text-2xl font-bold text-blue-500'>Python</li>
+      <li className='text-2xl font-bold text-green-500'>Java</li>
+      <li className='text-2xl font-bold text-yellow-500'>JavaScript</li>
+      <li className='text-2xl font-bold text-red-500'>React</li>
+    </ul>
     </>
   )
 }
